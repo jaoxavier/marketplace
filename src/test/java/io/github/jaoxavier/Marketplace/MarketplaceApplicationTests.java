65bfa4e7d36715ef152449e308e1b259d785cd15
jaoxavier <jvxavier.lima67@gmail.com>
2023-05-02 16:36:09 -0300

    first commit

---
+++ src/test/java/io/github/jaoxavier/Marketplace/MarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jaoxavier.Marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
